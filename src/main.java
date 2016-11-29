36b16a298e2d1bf6350c8d9fb07b5bb040dd6400
Ravi Bhankharia <ravibanks54@gmail.com>
2016-11-28 19:01:07 -0500

    working phase 1

--- src/main.java
+++ src/main.java
@@ -10,35 +10,31 @@ import java.util.Arrays;
 
 public class main {
         public static void main(String[] args) throws Exception {
+            String IV = "639404CBD1A1BD2322B206C39140";
+            String ciphertext = "5A052F928464CC3E437187ADCFC7E8F1CF9DEAC7059B5264E4E940D8C35AA60E2277D4832843043F593F40E4084609C886681BCF5B570D353BFF24C0E1F4A65E";
             int i = 0x0;
             for (; i < 0xF; i++){
-                System.out.println(i);
-                System.out.print(decrypt("000" + Integer.toHexString(i)));
+                //System.out.println(i);
+                System.out.print(decrypt(IV + "000" + Integer.toHexString(i), ciphertext));
             }
             i++;
             for (; i < 0xFF; i++){
-                System.out.println(i);
-                System.out.print(decrypt("00" + Integer.toHexString(i)));
+                //System.out.println(i);
+                System.out.print(decrypt(IV + "00" + Integer.toHexString(i), ciphertext));
             }
             i++;
             for (; i < 0xFFF; i++){
-                System.out.println(i);
-                System.out.print(decrypt("0" + Integer.toHexString(i)));
+                //System.out.println(i);
+                System.out.print(decrypt(IV + "0" + Integer.toHexString(i), ciphertext));
             }
             i++;
             for (; i < 0xFFFF; i++){
-                System.out.println(i);
-                System.out.print(decrypt(Integer.toHexString(i)));
+                //System.out.println(i);
+                System.out.print(decrypt(IV + Integer.toHexString(i), ciphertext));
             }
-
-
-
         }
 
-        public static String decrypt(String key) throws Exception{
-            String keyHex = "639404CBD1A1BD2322B206C39140" + key;
-            final String ciphertextHex = "5A052F928464CC3E437187ADCFC7E8F1CF9DEAC7059B5264E4E940D8C35AA60E2277D4832843043F593F40E4084609C886681BCF5B570D353BFF24C0E1F4A65E";
-
+        public static String decrypt(String keyHex, String ciphertextHex) throws Exception{
             SecretKey skey = new SecretKeySpec(DatatypeConverter
                     .parseHexBinary(keyHex), "AES");
 
@@ -47,10 +43,12 @@ public class main {
 
             byte[] result = cipher.doFinal(DatatypeConverter.parseHexBinary(ciphertextHex));
 
-            if (!isValid(result)){
+            Boolean res = isValid(result);
+            //System.out.println(res);
+            if (!res){
                 return "";
             }
-            return new String(result) + "\n";
+            return new String(result, "UTF-8") + "\n";
 
 
         }
@@ -58,11 +56,13 @@ public class main {
         public static boolean isValid(byte[] arr){
             int threshold = 0;
             for (int j = 0; j < arr.length; j ++){
-                if (arr[j] > 0x7E && arr[j] < 0x20){
+                //System.out.println(arr[j]);
+                if (arr[j] > 0x7E || arr[j] < 0x20){
                     threshold++;
                 }
             }
-            if (threshold > 1){
+            //System.out.println(threshold);
+            if (threshold > 20){
                 return false;
             }else{
                 return true;
