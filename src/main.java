272efa291f453421f6618ddcba249701280f6693
Ravi Bhankharia <ravibanks54@gmail.com>
2016-11-28 19:44:50 -0500

    broke ciphertext 1 and 2

--- src/main.java
+++ src/main.java
@@ -6,12 +6,27 @@ import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
-import java.util.Arrays;
 
 public class main {
         public static void main(String[] args) throws Exception {
-            final String IV = "639404CBD1A1BD2322B206C39140";
-            final String ciphertext = "5A052F928464CC3E437187ADCFC7E8F1CF9DEAC7059B5264E4E940D8C35AA60E2277D4832843043F593F40E4084609C886681BCF5B570D353BFF24C0E1F4A65E";
+            final long startTime = System.currentTimeMillis();
+            final String IV;
+            final String ciphertext;
+            int probNumber = 2;
+            if (probNumber == 1){
+                IV = "639404CBD1A1BD2322B206C39140";
+                ciphertext = "5A052F928464CC3E437187ADCFC7E8F1CF9DEAC7059B5264E4E940D8C35AA60E2277D4832843043F593F40E4084609C886681BCF5B570D353BFF24C0E1F4A65E";
+            }else if(probNumber == 2){
+                IV = "F806274AC0B446C18725ABDCE5";
+                ciphertext = "9D736AD64EFE153E6BEDE689772976ED83FB89D0503B27E7B4E2C4CDBE7B3BD9C1CE5E800D3929E543C3AD1B0D862990D7BCF77B74A126E27F5901EEFC5044BA";
+            }else if(probNumber == 3){
+                IV = "0AA4A910D451E069611D5571";
+                ciphertext = "574DD238070EC66A027F120B3D67A4B1FF20D1AAD52893CD2970E76BE73A2C4AE8AE87D1DC4CD4E6CE3733A27D401339E1E2A3FA9A0E86829284CACD5A850BCD";
+            }else{
+                IV = "9D0B180B5CD9DC074ACB0E";
+                ciphertext = "7102108459F8B9726887034491C1B409C29BF90CD1895B80815ABF2434DD57327CDFF16B9CF0C90C5F39CC92FC6EF99CDDE1D0FA90236F9474DF142B6BF1B64B";
+            }
+
             int keyLen = 32 - IV.length();  //Holds length of key after IV
             StringBuilder maxKey = new StringBuilder();
             StringBuilder padding = new StringBuilder();
@@ -29,26 +44,9 @@ public class main {
                 System.out.print(decrypt(IV + padding.toString() + hexString, ciphertext));
                 padding = new StringBuilder();  //clear padding
             }
-            /*
-            for (; i < 0xF; i++){
-                //System.out.println(i);
-                System.out.print(decrypt(IV + "000" + Integer.toHexString(i), ciphertext));
-            }
-            i++;
-            for (; i < 0xFF; i++){
-                //System.out.println(i);
-                System.out.print(decrypt(IV + "00" + Integer.toHexString(i), ciphertext));
-            }
-            i++;
-            for (; i < 0xFFF; i++){
-                //System.out.println(i);
-                System.out.print(decrypt(IV + "0" + Integer.toHexString(i), ciphertext));
-            }
-            i++;
-            for (; i < 0xFFFF; i++){
-                //System.out.println(i);
-                System.out.print(decrypt(IV + Integer.toHexString(i), ciphertext));
-            }*/
+            final long endTime = System.currentTimeMillis();
+            System.out.println("Total execution time: " + (endTime - startTime) );
+
         }
 
         public static String decrypt(String keyHex, String ciphertextHex) throws Exception{
@@ -71,15 +69,16 @@ public class main {
 
         public static boolean isValid(byte[] arr){
             int threshold = 0;
-            for (int j = 0; j < arr.length; j ++){
+            for (int j = 0; j < arr.length; j++){
                 if (arr[j] > 0x7E || arr[j] < 0x20){
-                    threshold++;
+                    if (threshold>20){
+                        return false;
+                    }else{
+                        threshold++;
+                    }
                 }
             }
-            if (threshold > 20){
-                return false;
-            }else{
-                return true;
-            }
+
+            return true;
         }
 }
